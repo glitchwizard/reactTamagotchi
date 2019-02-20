2402f9ed0822bbcd3eedbf0643370c81cc22d339
Charley McGowan and Sheila Stephen <charley@>
2019-02-20 08:54:42 -0800

    initial project set up

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Moment from 'moment'
+import AppBar from '@material-ui/core/AppBar'
+import { Switch, Route } from 'react-router-dom'
+
+function App(){
+
+    return (
+      <div>
+        <AppBar position="static">
+        </AppBar>
+          <h1>Hello !!!</h1>
+
+      </div>
+    )
+}
+export default App
